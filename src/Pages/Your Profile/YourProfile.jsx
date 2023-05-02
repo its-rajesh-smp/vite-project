16d8ddfebed4d5534e7c52330aa81e69ce337e13
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-02 22:21:09 +0530

    Password Can Be Changed

--- src/Pages/Your Profile/YourProfile.jsx
+++ src/Pages/Your Profile/YourProfile.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./YourProfile.css"
+import { API_KEY_CHANGEPASS } from '../../assets/assets';
 
 function YourProfile(props) {
+
+    const passRef = useRef()
+
+    const updatePassHandeler = async () => {
+        try {
+            const obj = {
+                idToken: JSON.parse(localStorage.getItem("ID_TOKEN")),
+                returnSecureToken: true,
+                password: passRef.current.value
+            }
+
+            const response = await fetch(API_KEY_CHANGEPASS, {
+                method: "POST",
+                body: JSON.stringify(obj),
+                headers: { "Content-Type": "application/json" }
+            })
+
+            const data = await response.json()
+            if (!response.ok) {
+                throw new Error(data.error.message)
+            }
+
+            // Save idToken in local Storage
+            localStorage.setItem("ID_TOKEN", JSON.stringify(data.idToken))
+
+            console.log(data);
+
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+
+
+
     return (
         <div className=' YourProfile-div '>
             <h1>Your Profile</h1>
             <p>Your Email</p>
             <p>email@gmail.com</p>
-            <input type="text" placeholder='Enter New Password' />
-            <button>Change Password</button>
+            <input ref={passRef} type="text" placeholder='Enter New Password' />
+            <button onClick={updatePassHandeler}>Change Password</button>
         </div>
     );
 }
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,3 +1,4 @@
 const API_KEY = "AIzaSyB5tDyejoKrBLQ3L2uGRvicKb2jLc-c-Rc"
 export const API_KEY_SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`
 export const API_KEY_SIGNIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`
+export const API_KEY_CHANGEPASS = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`
